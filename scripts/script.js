2b54e0cbacc7baaead7d3a31ca0ecb6fcdcfe465
George Benjamin <msdcconnect@gmail.com>
2018-06-19 21:56:42 +0100

    Added list on mouse click and enter key pressed

--- scripts/script.js
+++ scripts/script.js
@@ -4,6 +4,7 @@ var addTaskInput = document.getElementById("add_task_input");
 
 var toDoList = document.getElementById("to_do_list");
 
+
 function inputIsValid(){
     if(addTaskInput.value.length > 0){
         return true;
@@ -11,22 +12,37 @@ function inputIsValid(){
     return false;
 }
 
+function displayError(){
+    alert("Please enter an item.");
+}
+
 function addTask(){
     var li= document.createElement("li");
     li.appendChild(document.createTextNode(addTaskInput.value));
     toDoList.appendChild(li);
+    addTaskInput.value = "";
     
 }
 
 function addTaskOnClick(){
     if(inputIsValid()){
         addTask();
+    }else{
+        displayError();
     }
 }
 
-function addTaskOnEnterKeyPressed(){
+function addTaskOnEnterKeyPressed(event){
+    if(inputIsValid()){
+        if(event.which === 13){
+            addTask();
+        }
+    }
+    
     
 }
+
+
 
 addTaskButton.addEventListener("click", addTaskOnClick);
 
