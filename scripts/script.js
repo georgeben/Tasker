d57150e9bd5ef6bf9aa86feb9600fd01bd6239b3
George Benjamin <msdcconnect@gmail.com>
2018-06-20 10:17:46 +0100

    Added change bg colour and text colour feature

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,21 @@
+//iNITIALIZING VARIABLES
 var addTaskButton = document.getElementById("add_task_button");
 
 var addTaskInput = document.getElementById("add_task_input");
 
 var toDoList = document.getElementById("to_do_list");
 
+var bg_colour_btn = document.querySelector(".bg_colour_btn");
 
+var text_colour_btn = document.querySelector(".text_colour_btn");
+
+var note = document.querySelector(".container");
+//console.log(note);
+
+
+//FUNCTION DECLARATIONS
+
+//FUNCTION TO CHECK IF INPUT IS VALID
 function inputIsValid(){
     if(addTaskInput.value.length > 0){
         return true;
@@ -12,10 +23,13 @@ function inputIsValid(){
     return false;
 }
 
+
+//DISPLAYS AN ALERT IF THE INPUT IS INVALID
 function displayError(){
     alert("Please enter an item.");
 }
 
+//ADDS A TASK
 function addTask(){
     var li= document.createElement("li");
     li.appendChild(document.createTextNode(addTaskInput.value));
@@ -24,6 +38,7 @@ function addTask(){
     
 }
 
+//ADDS A TASK WHEN THE ADD TASK BUTTON IS CLICKED
 function addTaskOnClick(){
     if(inputIsValid()){
         addTask();
@@ -32,6 +47,7 @@ function addTaskOnClick(){
     }
 }
 
+//ADDS A TASK WHEN THE ENTER KEY IS PRESSED
 function addTaskOnEnterKeyPressed(event){
     if(inputIsValid()){
         if(event.which === 13){
@@ -42,8 +58,22 @@ function addTaskOnEnterKeyPressed(event){
     
 }
 
+function changeBgColour(){
+    note.style.background = bg_colour_btn.value;
+}
 
+function changeTextColour(){
+    note.style.color = text_colour_btn.value;
+
+}
+
+
+//EVENTS
 
 addTaskButton.addEventListener("click", addTaskOnClick);
 
 addTaskInput.addEventListener("keypress", addTaskOnEnterKeyPressed);
+
+bg_colour_btn.addEventListener("input", changeBgColour);
+
+text_colour_btn.addEventListener("input", changeTextColour);
